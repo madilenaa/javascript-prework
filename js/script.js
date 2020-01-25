f47cfefefa5db7de3b8ac862fb8c6c550edbc243
Magdalena Marchewka <madilenaa@gmail.com>
2020-01-25 22:23:26 +0100

    add computerMove

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-console.log('Gracz wpisał: ' + playerInput);
+let computerMove = 'nieznany ruch';
 
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if (playerInput=='2'){
-  playerMove = 'papier';
-} else (playerInput=='3'){
-  playerMove = 'nożyce';
+if(randomNumber == 1){
+  computerMove = 'kamień';
 }
-
-printMessage('Twój ruch to: ' + playerMove);
+else if (randomNumber == 2){
+computerMove = 'papier';
+}
+else (randomNumber == 3){
+computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);

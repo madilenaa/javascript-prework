a4c7a0ec34814aa3007f340acc32b14364850519
Magdalena Marchewka <madilenaa@gmail.com>
2020-01-25 22:29:47 +0100

    add playerMove

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
+//losowanie liczby od 1-3
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
-
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
@@ -13,3 +13,19 @@ else (randomNumber == 3){
 computerMove = 'nożyce';
 }
 printMessage('Mój ruch to: ' + computerMove);
+
+//wybieranie ruchu przez gracza
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if (playerInput == '2'){
+playerMove = 'papier';
+}
+else(playerInput == '3'){
+  playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
